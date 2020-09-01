4aa517be57639fa2123768a019ab47561b61a5ba
cecilpert <cecile.hilpert@gmail.com>
2020-09-01 17:03:35 +0200

    mail manager

---
+++ ts/src/mailManager.ts
@@ -0,0 +1,51 @@
+const Email = require('email-templates');
+const nodemailer = require('nodemailer');
+var ejs = require('ejs');
+
+let user, pass = null;
+let ibcpTransporter;
+let authorBot = 'cstb@ibcp.fr';
+
+export function configure(opt):void{
+    ibcpTransporter = nodemailer.createTransport({
+        host: 'smtp.ibcp.fr',
+        port: 587,
+        secure: false // true for 465, false for other ports
+       /* ,auth: {
+          user: user,
+          pass: pass 
+        }*/,
+        tls: {
+        // do not fail on invalid certs
+          rejectUnauthorized: false
+        }
+      });
+}
+
+export function send(adress:string, jobKey:string):void{
+    let data = { key : jobKey};
+  
+    let text = 'Dear user,\n\nYour job ' + jobKey + ' is completed.\n'
+              + 'You can access your results by following this link : http://crispr-dev.ibcp.fr:/results/' + jobKey + '\n\n\t\t\tThe CSTB Service.\n'
+              + '***This is an automatically generated email, please do not reply';
+    let subject = ejs.render('CSTB job <%= key %> completed', data);
+  
+  
+    let options = {
+      from: authorBot,
+      replyTo: authorBot,
+      to: adress,
+      subject: subject,
+      text: text
+    };
+  
+    ibcpTransporter.sendMail(options, 
+      (err, info)=> {
+        console.log(info.envelope);
+        console.log(info.message.Id);}
+    );
+        
+  }
+
+
+    
