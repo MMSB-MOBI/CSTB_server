7d74f512082b1f75663938adc58851e6c441167f
cecilpert <cecile.hilpert@gmail.com>
2020-10-21 11:21:38 +0200

    resolve conflict

--- ts/src/mailManager.ts
+++ ts/src/mailManager.ts
@@ -1,3 +1,5 @@
+import { rejects } from "assert";
+
 const Email = require('email-templates');
 const nodemailer = require('nodemailer');
 var ejs = require('ejs');
@@ -6,6 +8,9 @@ const utils = require("util");
 let user, pass = null;
 let ibcpTransporter;
 let authorBot = 'cstb@ibcp.fr';
+const support_mail = 'cstb-support@ibcp.fr'
+
+export type job_status = "complete" | "error"
 
 export function configure(opt):void{
     ibcpTransporter = nodemailer.createTransport({
@@ -23,24 +28,61 @@ export function configure(opt):void{
       });
 }
 
-export function send(adress:string, jobKey:string){
-    let data = { key : jobKey};
+function generateText(jobKey:string, status:job_status): string{
+  let text = `Dear user, \n\n Your job ${jobKey} `
+  if(status == "complete"){
+    logger.info("generateText status complete")
+    text = text + `is completed. \n You can access your results by following this link : http://crispr-dev.ibcp.fr:/results/${jobKey}***`
+  }
+  else if(status == "error"){
+    logger.info("generateText status error")
+    text = text + "ends with error. An email has automatically been send to support."
+  }
 
+  text = text + "\n\n\t\t\tThe CSTB Service.\n***This is an automatically generated email, please do not reply***"
+  return text
+}
+
+export function sendSupport(jobKey:string, user_mail:string|undefined){
+  const data = { key : jobKey};
+  const subject = ejs.render('[SUPPORT] CSTB job <%= key %> error', data);
+  const text = `The job ${jobKey} ends with an error.\nUser email : ${user_mail ? user_mail : "not provided"}\n\n**Automatic email***`
+  const options = {
+    from: authorBot,
+    replyTo: authorBot,
+    to: support_mail,
+    subject: subject,
+    text: text
+  };
   
-    let text = 'Dear user,\n\nYour job ' + jobKey + ' is completed.\n'
-              + 'You can access your results by following this link : http://cstb.ibcp.fr:/results/' + jobKey + '\n\n\t\t\tThe CSTB Service.\n'
-              + '***This is an automatically generated email, please do not reply';
-    let subject = ejs.render('CSTB job <%= key %> completed', data);
-  
+  return new Promise((resolve, reject) => {
+    ibcpTransporter.sendMail(options, 
+      (err, info)=> {
+        if(err){
+          reject(err)
+        }
+        else resolve(); 
+      }
+    );
+  })
+
+}
+
+export function send(address:string, jobKey:string, status:job_status){
+    let data = { key : jobKey};
+                
+    let subject = ejs.render('CSTB job <%= key %> ' + status, data);
+
+    const text = generateText(jobKey, status); 
   
     let options = {
       from: authorBot,
       replyTo: authorBot,
-      to: adress,
+      to: address,
       subject: subject,
       text: text
     };
-  
+
     return new Promise((resolve, reject) => {
       ibcpTransporter.sendMail(options, 
         (err, info)=> {
@@ -53,7 +95,6 @@ export function send(adress:string, jobKey:string){
         }
       );
     })
-    
         
   }
 
